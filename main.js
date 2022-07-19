94d1ae64f744491137387ca3f6df1c9153358d02
Cileb <Cileb@users.noreply.github.com>
2022-07-19 19:10:41 -0400

    Commit 2

---
+++ main.js
@@ -0,0 +1,13 @@
+// with git we've saved our code with different versions locally and on the cloud and we've copied code that others have wrote
+// git collaboration, how do we work on a project together?
+
+// How can I see if I already have git going in a project folder?
+// git status
+
+// git add . ; git commit -m "message"
+
+// git log --oneline
+
+// create new repo on GitHub
+// local git has no idea where our new repo on GitHub is
+// git remote to make the link
